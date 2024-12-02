1de3fdbbc92f7778ae1d28e64c0a819d14d8a03b
RPrasetyoB <rpbasuki.dev@gmail.com>
2024-12-02 13:43:54 +0700

    style: payment-success card

--- app/(defaults)/payment-success/page.tsx
+++ app/(defaults)/payment-success/page.tsx
@@ -21,12 +21,18 @@ const PaymentSuccess = () => {
     return (
         <div className="flex min-h-[750px] w-full items-center justify-center">
             <div className="mx-auto flex h-[250px] w-[320px] flex-col items-center justify-around rounded-md bg-white/60 py-10 backdrop-blur-3xl dark:bg-black/50">
-                <h1 className="dark:text-shadow-dark-mode text-lg font-extrabold uppercase !leading-snug text-primary md:text-xl">
-                    Pembayaran Berhasil!
-                </h1>
-                <div className="flex flex-col items-center gap-2">
-                    <p className="font-bold dark:text-white md:text-lg">Mengalihkan setelah:</p>
-                    <p className="font-bold text-dark dark:text-white md:text-lg">{countdown} detik</p>{' '}
+                <div className="mx-auto flex h-[300px] w-full flex-col items-center justify-around">
+                    <h1 className="bg-gradient-to-r from-yellow-300 to-yellow-500 bg-clip-text text-xl font-extrabold !leading-snug text-transparent md:text-2xl">
+                        Payment Success !
+                    </h1>
+                    <div className="flex flex-col items-center justify-center gap-2">
+                        <p className="text-md mt-2 text-center text-dark dark:text-white">
+                            You will be redirected in a few seconds.
+                        </p>
+                        <div>
+                            <p className="font-bold text-dark dark:text-white md:text-lg">{countdown} seconds</p>{' '}
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
