8d230aff719858f475d53ba1204f16e1764daf0e
Sherin Olivia <sochronicle@gmail.com>
2024-12-21 14:56:29 +0700

    feat: create currency format without 'Rp'

--- utils/curency-format.ts
+++ utils/curency-format.ts
@@ -7,3 +7,13 @@ export function formatToRupiah(amount: number | undefined): string {
         maximumFractionDigits: 0,
     })}`;
 }
+
+export function formatMataUang(amount: number | undefined): string {
+    if (!amount) {
+        return '';
+    }
+    return `${amount.toLocaleString('id-ID', {
+        minimumFractionDigits: 0,
+        maximumFractionDigits: 0,
+    })}`;
+}
