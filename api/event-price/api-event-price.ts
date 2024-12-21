d8f9c0716646a5045d398353de1a3baff56fc87b
Sherin Olivia <sochronicle@gmail.com>
2024-12-21 14:58:18 +0700

    feat: created integration to delete and update

--- api/event-price/api-event-price.ts
+++ api/event-price/api-event-price.ts
@@ -14,9 +14,51 @@ export const getAllEventPrice = async (token: string | undefined) => {
     }
 };
 
-export const updateEvent = async (eventId: string, data: EventUpdateType, cookie: string | undefined) => {
+export const getOneEventPrice = async (event_price_id: string, token: string | undefined) => {
     try {
-        const response = await api.patch(`/events/${eventId}`, data, {
+        const response = await api.get(`/event-prices/${event_price_id}`, {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        });
+        return response.data;
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
+};
+
+export const createEventPrice = async (data: EventPriceRegisterType, cookie: string | undefined) => {
+    try {
+        const response = await api.post(`/event-prices`, data, {
+            headers: {
+                Authorization: `Bearer ${cookie}`,
+            },
+        });
+        return response.data;
+    } catch (error) {
+        console.error(error);
+        return error;
+    }
+};
+
+export const deleteEventPrice = async (event_price_id: string, cookie: string | undefined) => {
+    try {
+        const response = await api.delete(`/event-prices/${event_price_id}`,{
+            headers: {
+                Authorization: `Bearer ${cookie}`,
+            },
+        });
+        return response.data;
+    } catch (error) {
+        console.error(error);
+        return error;
+    }
+};
+
+export const updateEventPrice = async (event_price_id: string, data: EventPriceRegisterType, cookie: string | undefined) => {
+    try {
+        const response = await api.patch(`/event-prices/${event_price_id}`, data, {
             headers: {
                 Authorization: `Bearer ${cookie}`,
             },
